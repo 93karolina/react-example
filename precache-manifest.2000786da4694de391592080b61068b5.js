cb971f320d9efbdb045da600a9d5fa51d34616af
93karolina <62308919+93karolina@users.noreply.github.com>
2020-06-06 16:35:21 +0200

    Updates

---
+++ precache-manifest.2000786da4694de391592080b61068b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0adfa5d0e667aef78a492e3580f62ce",
+    "url": "/react-example/index.html"
+  },
+  {
+    "revision": "5cd0eb9e62ab4c502b83",
+    "url": "/react-example/static/css/main.8cdb23e2.chunk.css"
+  },
+  {
+    "revision": "7dd0288dac562ada2083",
+    "url": "/react-example/static/js/2.98aa46ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-example/static/js/2.98aa46ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cd0eb9e62ab4c502b83",
+    "url": "/react-example/static/js/main.c9974fa6.chunk.js"
+  },
+  {
+    "revision": "91a7da182e845a375242",
+    "url": "/react-example/static/js/runtime-main.73f734a8.js"
+  }
+]);
